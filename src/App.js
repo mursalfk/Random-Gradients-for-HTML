7936a9a4a82ddba90fe767dd6d2f6a7905267526
Mursal Furqan <mursalfurqan@gmail.com>
2022-05-27 12:26:34 +0500

    Changes in Code

--- src/App.js
+++ src/App.js
@@ -61,24 +61,14 @@ function App() {
     draggable: true,
     progress: undefined,
   });
+
+  const copyToClipboard = (param, name) => {
+    navigator.clipboard.writeText(param)
+    notify(name)
+  }
   return (
     <>
-      {/* Show this component only in mobile */}
-      {/* {window.innerWidth < 768 ? (
-        <>
-          <button
-            onClick={() => { shuffleFunc() }}
-            className="mobile_shuffle_button"
-          > */}
-      {/* ♻️ */}
-      {/* icon for shuffle */}
-      {/* <svg className="mobile_shuffle_icon" viewBox="0 0 24 24">
-              <path d="M0 0h24v24H0z" fill="none" />
-              <path d="M10.59 9.17L5.41 4 4 5.41l5.17 5.17 1.42-1.41zM14.5 4l2.04 2.04L4 18.59 5.41 20 17.96 7.46 20 9.5V4h-5.5zm.33 9.41l-1.41 1.41 3.13 3.13L14.5 20H20v-5.5l-2.04 2.04-3.13-3.13z" />
-            </svg>
-          </button>
-        </>
-      ) : (<></>)} */}
+
       <div className="color_cabin">
         <p className='colorTitle'>Generate Random Gradient</p>
         <p className='init_instruction0'>
@@ -86,76 +76,61 @@ function App() {
           <br />⏬🔽⬇️👇👇👇⬇️🔽⏬
         </p>
       </div>
+
       {/* Area to show the random values */}
       <div className="random_values">
-        {/* In a Table */}
         <table>
           <thead>
             <tr>
               <th colSpan='2'>Details <span className='clickToCopy'>(Click to copy)</span></th>
             </tr>
           </thead>
+
           <tbody>
-            <tr>
+            <tr onClick={() => copyToClipboard(randomColor_state, 'Random Color 1')}>
               <th>Random Color 1</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(randomColor_state)
-                notify('Random Color 1')
-              }}>{randomColor_state}</td>
+              <td>{randomColor_state}</td>
             </tr>
-            <tr>
+
+            <tr onClick={() => copyToClipboard(randomColor2_state, 'Random Color 2')}>
               <th>Random Color 2</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(randomColor2_state)
-                notify('Random Color 2')
-              }}>{randomColor2_state}</td>
+              <td>{randomColor2_state}</td>
             </tr>
-            <tr>
+
+            <tr onClick={() => copyToClipboard(gradientType_state, 'Gradient Type')}>
               <th>Gradient Type</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(gradientType_state)
-                notify('Gradient Type')
-              }}>{gradientType_state}</td>
+              <td>{gradientType_state}</td>
             </tr>
-            <tr>
+
+            <tr onClick={() => copyToClipboard(gradientDirection_state, 'Gradient Direction')}>
               <th>Gradient Direction</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(gradientDirection_state)
-                notify('Gradient Direction')
-              }}>{gradientDirection_state}</td>
+              <td>{gradientDirection_state}</td>
             </tr>
-            <tr>
+
+            <tr onClick={() => copyToClipboard(gradientPosition_state, 'Gradient Position')}>
               <th>Gradient Position</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(gradientPosition_state)
-                notify('Gradient Position')
-              }}>{gradientPosition_state}</td>
+              <td>{gradientPosition_state}</td>
             </tr>
-            <tr>
-              <th>Random Gradient</th>
-              <td onClick={() => {
-                navigator.clipboard.writeText(randomGradient_state);
-                notify('Random Gradient')
-              }}>{randomGradient_state}</td>
+
+            <tr onClick={() => copyToClipboard(randomGradient_state, 'Gradient Code')}>
+              <th>Gradient Code</th>
+              <td>{randomGradient_state}</td>
             </tr>
           </tbody>
         </table>
 
       </div>
-      {/* {window.innerWidth >= 768 ? ( */}
-
       <button
         onClick={() => { shuffleFunc() }}
         className="shuffle_button"
       >
-        {/* Add Icon for Shuffle */}
-        {/* <svg className="shuffle_icon" width="20" height="20" fill="white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-6 400H54c-3.3 0-6-2.7-6-6V86c0-3.3 2.7-6 6-6h404c3.3 0 6 2.7 6 6v340c0 3.3-2.7 6-6 6zm-42-324H150v200h200V64zm0 240H150v84h200v-84zM192 80h224v80H192V80z" /></svg> */}
         Generate New Gradient
       </button>
-      {/* ) : (<></>)} */}
+
       <div className='footer'>
         Created with ❤️ by <a href='https://mursalfk.github.io/' target='_blank' rel='noopener noreferrer'>Mursal Furqan</a>
       </div>
+
       <ToastContainer
         position="bottom-left"
         autoClose={2000}
